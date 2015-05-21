3be6ded5fe6eb13973a142159d1c8c7bdb985b53
Scott Frederick <scottyfred@gmail.com>
2015-05-21 13:03:56 -0500

    Added nested config class.

--- src/main/java/org/example/test/TestApplication.java
+++ src/main/java/org/example/test/TestApplication.java
@@ -2,10 +2,18 @@ package org.example.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.java.AbstractCloudConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class TestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
+	}
+
+	@Profile("nested")
+	@Configuration
+	public class NestedCloudConfig extends AbstractCloudConfig {
 	}
 }
